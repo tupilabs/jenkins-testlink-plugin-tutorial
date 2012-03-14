9c56c39d17b2ba34a2c7d669865fdb5e0d9deaf1
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2012-03-14 20:10:59 -0300

    Updating book for release 3.1

--- src/test/java/jenkins/plugins/testlink/examples/TestCurrentTime.java
+++ src/test/java/jenkins/plugins/testlink/examples/TestCurrentTime.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright (c) <2011> <Bruno P. Kinoshita>
+ * Copyright (c) <2012> <Bruno P. Kinoshita>
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package hudson.plugins.testlink.examples;
+package jenkins.plugins.testlink.examples;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -30,7 +30,7 @@ import org.testng.annotations.Test;
  * Tests System.currentTimeMillis()
  * 
  * @author Bruno P. Kinoshita - http://www.kinoshita.eti.br
- * @since 0.2
+ * @since 0.3
  */
 public class TestCurrentTime
 {
